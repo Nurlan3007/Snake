67f5ee147589f3e9ca7e24793bb4bcd2ecebf7e8
Nurlan Marat <69515018+Nurlan3007@users.noreply.github.com>
2020-12-04 14:42:24 +0600

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,186 +1,132 @@
-# CREATE index: 16.06.2020
-# USER: Nurlan
-
-############## ПЛАН ##############
-# 1: Создать поля
-# 2: Нарисовать змейку
-# 3: Научить змейку ходить
-# 4: Написать проверки на нахождение змейки в полях
-# 5: Создавать яблоки в рандомных местах
-# 6: Написать проверки на нахождение змейки в полях
-# и т.д...
-############################
-
-# Библиотеки которые нам нажны
 import pygame
-import random as r
-import sys
+from sys import exit
+from random import randrange
+import sqlite3
+
+
+def connect_db():
+    global db,cursor
+    db = sqlite3.connect("server")
+    cursor = db.cursor()
+
+# Functions
+def saveInDatabase(count):
+    connect_db()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS score (
+        id integer primary key AUTOINCREMENT,
+        count varchar(150) DEFAULT 0
+    )''')
+
+    db.commit()
+
+    cursor.execute("SELECT count FROM score")
+    score = cursor.fetchall()
+    if count > score[0][0]:
+        cursor.execute("UPDATE score SET count = ?",[count])
+        db.commit()
+
+connect_db()    
+highScore = cursor.execute("SELECT count FROM score")
+highScore = cursor.fetchall()
+highScore = highScore[0][0]
+
+# Options
+colors = {
+    'black': (0, 0, 0),
+    'white': (255, 255, 255),
+    'red' : (255, 0, 0),
+    'blue': (0, 0, 255),
+    'green': (0, 200, 0),
+}
+
+MARGIN = 1
+SIZE_BLOCK = 20
+COUNT_BLOCKS = 20
+
+WIDTH = SIZE_BLOCK * COUNT_BLOCKS
+HEIGHT = WIDTH + 100
+
+snakeX = 180
+snakeY = 180 + 100
+snake = [[snakeX,snakeY]]
+dx,dy = 0,0
+
+appleX,appleY = randrange(0,WIDTH,20),randrange(100,HEIGHT,20)
+
+count,length = 0,2
 
 pygame.init()
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption("Snake")
 
-# Start window
-sizeWindow = [450,500]
-nameGame = 'Snake Pygame'
-window = pygame.display.set_mode(( sizeWindow[0],sizeWindow[1] ))
-pygame.display.set_caption(nameGame)
+while True:
+    pygame.time.delay(120)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            length -= 2
+            saveInDatabase(length)
+            pygame.quit()
+            exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and dx == 0:
+                dx,dy = -20,0
+            elif event.key == pygame.K_RIGHT and dx == 0:
+                dx,dy = 20,0
+            elif event.key == pygame.K_UP and dy == 0:
+                dx,dy = 0,-20
+            elif event.key == pygame.K_DOWN and dy == 0:
+                dx,dy = 0,20
 
+    pygame.draw.rect(screen, colors['white'], [0, 0, WIDTH, 100])    
 
-#### Colors ####
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED   = (108, 0, 0)
-BLUE  = (52, 82, 235)
-GREEN = (131, 212, 138)
-FRAME = (52, 220, 235)
-################
+    for row in range(COUNT_BLOCKS):
+        for col in range(COUNT_BLOCKS):
+            if (row + col) % 2 == 0:
+                color = (175,171,255)
+            else:
+                color = (175,116,253)
+            x = row * SIZE_BLOCK
+            y = col * SIZE_BLOCK + 100
+            pygame.draw.rect(screen, color, [x, y, SIZE_BLOCK, SIZE_BLOCK])
 
-COUNT_BLOCKS = 20
-SIZE_BLOCK = 20
-margin = 1
+    snakeX += dx
+    snakeY += dy
+    snake.append([snakeX,snakeY])
+    snake = snake[-length:]
+    body = snake[:-2]
+    head = snake[-1]
 
-# FUNCTIONS and CLASS
-class SnakeBlocks:
-	def __init__(self,x,y):
-		self.x = x
-		self.y = y
-	def isInside(self):
-		if 0 <= self.x < COUNT_BLOCKS and 0 <= self.y < COUNT_BLOCKS:
-			return True
-		else:
-			return False
-	def __eq__(self, other):
-		return isinstance(other,SnakeBlocks) and other.x == self.x and other.y == self.y
+    if head[0] == appleX and head[1] == appleY:
+        count += 1
+        length += 1
+        appleX,appleY = randrange(0,WIDTH,20),randrange(100,HEIGHT,20)
 
+    if head[0] < 0:
+        snakeX = WIDTH - 20 
+    elif head[0] > WIDTH:
+        snakeX = 0
+    elif head[1] < 120:
+        snakeY = HEIGHT
+    elif head[1] > HEIGHT:
+        snakeY = 100
 
-def randomSpawn():
-	x = r.randint(0,COUNT_BLOCKS - 1)
-	y = r.randint(0,COUNT_BLOCKS - 1)
-	random_spawn = SnakeBlocks(x,y)
-	return random_spawn
+    for i,j in body:
+        if i == head[0] and j == head[1]:
+            length -= 2
+            saveInDatabase(length)
+            pygame.quit()
+            exit()
 
-def draw_blocks(color,column,row):
-	pygame.draw.rect(
-		window,
-		color,
-		[
-			10 + column * COUNT_BLOCKS + margin * (column + 1),  # postion X
-			20 + row * COUNT_BLOCKS + margin * (row + 1),  # positon Y
-			SIZE_BLOCK,
-			SIZE_BLOCK,
-		]
-	)
-def write(text,color,x,y,size):
-	font = pygame.font.SysFont('serif',size)
-	text = font.render(text,1,color)
-	window.blit(text,(x,y))
+    for i,j in snake:
+        pygame.draw.rect(screen, colors['green'], [i, j, SIZE_BLOCK, SIZE_BLOCK])
+    pygame.draw.rect(screen, colors['red'], [appleX, appleY, SIZE_BLOCK, SIZE_BLOCK]) 
+    
+    font = pygame.font.SysFont('sans-serif',35)
+    text1 = font.render("Score: " + str(count),True,colors['black'])
+    text2 = font.render("High Score: " + str(highScore),True,colors['black'])
+    screen.blit(text1,(20,20))
+    screen.blit(text2,(20,60))       
 
-###############################################
-
-speed = 300
-total = 0
-
-apple = randomSpawn()
-snake_blocks = [
-	SnakeBlocks(9,9),
-	SnakeBlocks(10,9),
-	SnakeBlocks(11, 9),
-]
-
-d_row = 0
-d_column = 1
-
-run = True
-while run:
-	pygame.time.delay(speed)
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT and d_row != 0:
-				d_row = 0
-				d_column = -1
-			if event.key == pygame.K_RIGHT and d_row != 0:
-				d_row = 0
-				d_column = 1
-			if event.key == pygame.K_UP and d_column != 0:
-				d_row = -1
-				d_column = 0
-			if event.key == pygame.K_DOWN and d_column != 0:
-				d_row = 1
-				d_column = 0
-
-	window.fill(WHITE)
-	########### TEXT ############
-	write("Total: " + str(total) + ", ",BLACK,20,460,30)
-	write("Speed: " + str(speed),BLACK, 150, 460, 30)
-
-	# Создание полей
-	for column in range(COUNT_BLOCKS):
-		for row in range(COUNT_BLOCKS):
-			if (column + row) % 2 == 0:
-				color = ((152, 222, 227))
-			else:
-				color = ((46, 204, 217))
-			draw_blocks(color,column,row)
-
-	head = snake_blocks[-1]
-
-	# Есть ли змейка в поле
-	if head.isInside() == False:
-		print("\033[31m {}".format('CRASH'))
-		sys.exit()
-
-	# Рисуем яблоко
-	draw_blocks(RED,apple.x,apple.y)
-	if apple == head:
-		total += 1
-		snake_blocks.append(apple)
-		apple = randomSpawn()
-		draw_blocks(RED, apple.x, apple.y)
-
-	if total % 5 == 0 and total != 0 and speed >= 130:
-		speed -= 40
-
-
-	for block in snake_blocks:
-		x = block.x
-		y = block.y
-		draw_blocks(BLACK,x,y)
-
-	new_head = SnakeBlocks(head.x + d_column,head.y + d_row)
-
-	if new_head in snake_blocks:
-		print("\033[31m {}".format('CRASH'))
-		sys.exit()
-
-	snake_blocks.append(new_head)
-	snake_blocks.pop(0)
-
-
-	pygame.display.update()
-
-# Выход из pygame
-pygame.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    pygame.display.update()
 
 
